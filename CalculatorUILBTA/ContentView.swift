bad268255681d33361e683d21c355e66ba8a653f
Dylan LUCHMUN <dylan.luchmun@epitech.eu>
2020-01-09 13:25:17 +0100

    réactivité bouton

--- CalculatorUILBTA/ContentView.swift
+++ CalculatorUILBTA/ContentView.swift
@@ -51,7 +51,18 @@ enum CalculatorButton: String {
     
 }
 
+//Env object
+class GlobalEnvironment: ObservableObject {
+    @Published var display = ""
+    
+    func receiveInput(calculatorButton: CalculatorButton) {
+        self.display = calculatorButton.title
+    }
+}
+
 struct ContentView: View {
+    
+    @EnvironmentObject var env: GlobalEnvironment
     
     let buttons: [[CalculatorButton]] = [
         [.ac, .plusMinus, .percent, .divide],
@@ -68,31 +79,42 @@ struct ContentView: View {
             VStack (spacing: 12){
                 HStack {
                     Spacer()
-                    Text("42").foregroundColor(.white)
+                    Text(env.display ).foregroundColor(.white)
                         .font(.system(size: 64))
                 }.padding()
                 
                 ForEach(buttons, id: \.self) { row in
                     HStack (spacing: 12) {
                         ForEach(row, id: \.self){ button in
-                            
-                            Button(action: {
-                                
-                            }) {
-                                 Text(button.title)
-                                .font(.system(size: 32))
-                                    .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
-                                    .foregroundColor(.white)
-                                    .background(button.backgroundColor)
-                                    .cornerRadius(self.buttonWidth(button: button))
-                            }
+                            CalculatorButtonView(button: button)
                         }
                     }
                 }
             }.padding(.bottom)
         }
     }
-    func buttonWidth(button: CalculatorButton) -> CGFloat {
+}
+
+struct CalculatorButtonView: View {
+    
+    var button: CalculatorButton
+    
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    var body: some View {
+        Button(action: {
+            self.env.receiveInput(calculatorButton: self.button)
+        }) {
+             Text(button.title)
+            .font(.system(size: 32))
+                .frame(width: self.buttonWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                .foregroundColor(.white)
+                .background(button.backgroundColor)
+                .cornerRadius(self.buttonWidth(button: button))
+        }
+    }
+    
+    private func buttonWidth(button: CalculatorButton) -> CGFloat {
         
         if button == .zero {
            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
@@ -103,6 +125,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GlobalEnvironment())
     }
 }
